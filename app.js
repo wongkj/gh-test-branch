50e7067c3e2bf2ae94c3a231c325b26374359fa7
Jason Wong <wongkj.11@gmail.com>
2023-11-30 21:42:26 +1100

    feat: Added app code and test

---
+++ app.js
@@ -0,0 +1,40 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json());
+
+const port = 7070
+
+let users = []
+
+app.get('/users', (req, res) => {
+    res.status(200).send(users)
+})
+
+app.get('/users/:id', (req, res) => {
+    const user = users.find(user => user.id === parseInt(req.params.id))
+    console.log({ user })
+    res.status(200).send(user)
+})
+
+app.post('/users', (req, res) => {
+    const body = req.body
+    const { name, age } = body
+    const newUser = {
+        id: users.length + 1,
+        name,
+        age: parseInt(age)
+    }
+    users.push(newUser)
+    res.status(201).send(newUser)
+})
+
+app.delete('/users/:id', (req, res) => {
+    users = users.filter(user => user.id !== parseInt(req.params.id))
+    res.status(200).send({ message: 'user deleted' })
+})
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
---
+++ app.test.js
@@ -0,0 +1,5 @@
+describe('random test', () => {
+    test('passing test', async () => {
+        expect(true).toBeTruthy()
+    })
+})
